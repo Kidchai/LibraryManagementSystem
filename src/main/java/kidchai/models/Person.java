19276e3fc038a6da47424ec1ae32a725836d50dc
Kidchai <o.naibova@gmail.com>
2023-01-17 18:26:47 -0600

    Create project library1

---
+++ src/main/java/kidchai/models/Person.java
@@ -0,0 +1,38 @@
+package kidchai.models;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public Person(){}
+    public Person(int id, String name, int yearOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+    private int yearOfBirth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
