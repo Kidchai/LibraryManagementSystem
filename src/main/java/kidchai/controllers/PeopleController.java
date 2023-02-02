8c07dda9da302405b4927ad67a35ac36e3c5511b
Kidchai <o.naibova@gmail.com>
2023-02-02 13:37:01 -0600

    Add 'delete' and 'add' abilities to person

--- src/main/java/kidchai/controllers/PeopleController.java
+++ src/main/java/kidchai/controllers/PeopleController.java
@@ -1,12 +1,13 @@
 package kidchai.controllers;
 
+import jakarta.validation.Valid;
 import kidchai.dao.PersonDao;
+import kidchai.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -28,5 +29,26 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDao.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") @Valid Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "people/index";
     }
 }
