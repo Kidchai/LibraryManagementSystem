d6178dcdce9f5fed869253960b50b7cce0c6b4a3
Kidchai <o.naibova@gmail.com>
2023-03-16 22:55:12 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+Project for book management at library. Web-application for librarians.
+
+---
+
+This is the first part of my implementation of this management system with the same functionality but different libraries.
+
+[Library1]() stack: Java, Spring MVC, JDBC, PostgreSQL, Thymeleaf, HTML
+
+---
+
+### Librarians can:
+- Create, delete, and edit book records
+- Register new people as customers
+- Edit information about customers, and delete customers' profiles
+- Borrow books to customers
+- Mark returned books as available
+
+### How does it work?
+Look
+![gif file with demonstration of app work](forReadme/library.gif)
--- src/main/resources/populateTablesForTests.sql
+++ src/main/resources/populateTablesForTests.sql
@@ -1,9 +1,9 @@
 INSERT INTO people (name, birth_year)
-VALUES ('Ivanov Ivan Ivanovich', 1976);
+VALUES ('Andrew Finch', 1976);
 INSERT INTO people (name, birth_year)
-VALUES ('Petrov Petr Petrovich', 1986);
+VALUES ('Lindsey Garcia', 1988);
 INSERT INTO people (name, birth_year)
-VALUES ('Markov Mark Markovich', 1996);
+VALUES ('Olivia Jones', 1997);
 
 INSERT INTO books (person_id, title, author, year)
 VALUES (2, 'To Kill a Mockingbird', 'Harper Lee', 1960);
@@ -12,8 +12,8 @@ VALUES (NULL, 'Atomic Habits', 'James Clear', 2018);
 INSERT INTO books (person_id, title, author, year)
 VALUES (2, 'A Brief History of Time', 'Stephen Hawking', 1988);
 INSERT INTO books (person_id, title, author, year)
-VALUES (3, 'Crime and Punishment', 'Fyodor Dostoyevsky', 1866);
+VALUES (3, 'Crime and Punishment', 'Fyodor Dostoevsky', 1866);
 INSERT INTO books (person_id, title, author, year)
-VALUES (NULL, 'Lullaby', 'Chuck Palahniuk', 2002);
+VALUES (NULL, 'Anna Karenina', 'Leo Tolstoy', 1877);
 INSERT INTO books (person_id, title, author, year)
 VALUES (NULL, 'The Lord of the Rings Novel', 'J. R. R. Tolkien', 1954);
