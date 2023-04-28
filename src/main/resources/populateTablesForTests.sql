a6180fd559209153a3cc7bbc9afde139b6347718
Kidchai <o.naibova@gmail.com>
2023-04-28 16:11:33 -0500

    Add the checking book for overdue.
    If the book is overdue, it will be marked red in the person's book list

--- src/main/resources/populateTablesForTests.sql
+++ src/main/resources/populateTablesForTests.sql
@@ -5,15 +5,15 @@ VALUES ('Lindsey Garcia', '1976-03-05');
 INSERT INTO people (name, birth_year)
 VALUES ('Olivia Jones', '2003-08-21');
 
-INSERT INTO books (person_id, title, author, year)
-VALUES (2, 'To Kill a Mockingbird', 'Harper Lee', 1960);
+INSERT INTO books (person_id, title, author, year, taken_at)
+VALUES (2, 'To Kill a Mockingbird', 'Harper Lee', 1960, '2023-03-27');
 INSERT INTO books (person_id, title, author, year)
 VALUES (NULL, 'Atomic Habits', 'James Clear', 2018);
-INSERT INTO books (person_id, title, author, year)
-VALUES (2, 'A Brief History of Time', 'Stephen Hawking', 1988);
-INSERT INTO books (person_id, title, author, year)
-VALUES (3, 'Crime and Punishment', 'Fyodor Dostoevsky', 1866);
-INSERT INTO books (person_id, title, author, year)
-VALUES (NULL, 'Anna Karenina', 'Leo Tolstoy', 1877);
+INSERT INTO books (person_id, title, author, year, taken_at)
+VALUES (2, 'A Brief History of Time', 'Stephen Hawking', 1988, '2023-04-19');
+INSERT INTO books (person_id, title, author, year, taken_at)
+VALUES (3, 'Crime and Punishment', 'Fyodor Dostoevsky', 1866, '2023-01-03');
+INSERT INTO books (person_id, title, author, year, taken_at)
+VALUES (NULL, 'Anna Karenina', 'Leo Tolstoy', 1877, '2023-04-02');
 INSERT INTO books (person_id, title, author, year)
 VALUES (NULL, 'The Lord of the Rings Novel', 'J. R. R. Tolkien', 1954);
